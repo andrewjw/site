5b3750d6f4be8ec3f16dd42d1efbe703f0c1d49a
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-02-03 21:56:35 +0000

    Fix typo.

--- buildbot.sh
+++ buildbot.sh
@@ -9,6 +9,6 @@ bundle install
 bundle exec jekyll build
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$BRANCH" != "master" ]]; then
+if [[ "$BRANCH" == "master" ]]; then
   rsync -avc --delete _site/ $SSH_USER@$SSH_HOST:site/
 fi
