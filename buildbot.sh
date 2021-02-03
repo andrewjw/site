b9eb9406d3aed70469ededc504ae54cd0b86adde
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-02-03 21:53:16 +0000

    Only deploy if we're on master.

--- buildbot.sh
+++ buildbot.sh
@@ -8,4 +8,7 @@ bundle install
 ./run_checks.sh
 bundle exec jekyll build
 
-rsync -avc --delete _site/ $SSH_USER@$SSH_HOST:site/
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" != "master" ]]; then
+  rsync -avc --delete _site/ $SSH_USER@$SSH_HOST:site/
+fi
