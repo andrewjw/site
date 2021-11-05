7bedc76a2212542b0bd331d9a59ab64f14b61dc9
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2021-11-05 17:07:31 +0000

    Add unsplash keys.

--- check_file.py
+++ check_file.py
@@ -11,7 +11,7 @@ front_matter_re = re.compile("---\n(.*?)---\n", re.M | re.DOTALL)
 allowed_keys = set([
     "layout", "title", "date", "tags", "permalink",
     "flickr_user", "flickr_username", "flickr_image", "flickr_imagelink", "flickr_imagename",
-    "flickr"
+    "flickr", "unsplash_image", "unsplash_title", "unsplash_url", "unsplash_user"
 ])
 
 def check_file(fn):
