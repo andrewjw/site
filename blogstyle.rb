f8f96d6fbce7bf0c8cbd69f3d738b5966e9294c2
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2023-11-02 14:20:06 +0000

    Update blogstyle.rb to match change to mdl.

--- blogstyle.rb
+++ blogstyle.rb
@@ -7,4 +7,4 @@ rule 'MD013', :line_length => 160
 exclude_rule 'MD026'
 exclude_rule 'MD036'
 exclude_rule 'MD041'
-rule 'MD033', :allowed_elements => ["sup", "sub"]
+rule 'MD033', :allowed_elements => "sup,sub"
